d9285bec8c1da79a8e49feb9ef13ea319431cbab
Wahid Sadique Koly <wskoly.bp@gmail.com>
2024-07-03 10:46:16 +0600

    check by video

--- VimeoAutomation.py
+++ VimeoAutomation.py
@@ -1,3 +1,4 @@
+from requests import get
 import vimeo
 import os
 import csv
@@ -6,6 +7,7 @@ import json
 
 logging.basicConfig(filename='vimeo.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
+FOLDER_IDS = {"CHITTAGONG":"21407340"}
 CONFIG = {}
 with open('config.json', 'r') as f:
     CONFIG = json.load(f)
@@ -24,7 +26,8 @@ def k_print(*args, **kwargs):
         elif kwargs['log_level'] == 'error':
             logging.error(args)
         kwargs.pop('log_level')
-    print('\r',*args, **kwargs, end='\r')
+    # print('\r',*args, **kwargs, end='\r')
+    print(*args, **kwargs)
 
 
 
@@ -89,6 +92,75 @@ def upload_multiple_videos(master_csv_path, video_dir, *args, **kwargs):
     # res = vClient.put('/me/projects/21389077/videos', data={'uris': ", ".join(video_uris)})
     # print(res.json())
 
+def get_master_csv_data(master_csv_path):
+    data_dict = []
+    with open(master_csv_path, 'r', encoding='utf-8') as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            data_dict.append(row)
+    return data_dict
+
+
+def get_videos_info(*args, **kwargs):
+    matched_output_dir = kwargs.get('matched_output_dir', os.path.join(os.getcwd(), 'outputs' ,'matched_videos'))
+    unmatched_output_dir = kwargs.get('unmatched_output_dir', os.path.join(os.getcwd(), 'outputs' ,'unmatched_videos'))
+
+    os.makedirs(matched_output_dir, exist_ok=True)
+    os.makedirs(unmatched_output_dir, exist_ok=True)
+    master_csv_path = kwargs.get('master_csv_path', None)
+    location = kwargs.get('location', None)
+    if not master_csv_path or not location:
+        k_print(f'Master CSV path and location are required kwargs: master_csv_path, location', log_level='error')
+        return
+    
+    master_data = get_master_csv_data(master_csv_path)
+    uri = f'/me/projects/{FOLDER_IDS[location.upper()]}/videos'
+    res = vClient.get(f'{uri}?per_page=100')
+    if res.status_code != 200:
+        k_print(f'Failed to get videos for location: {location}', log_level='error')
+        return
+    res_json = res.json()
+    total_videos = res_json['total']
+
+    next_page = res_json['paging']['next']
+    data = res_json["data"]
+    while next_page:
+        res = vClient.get(next_page)
+        res_json = res.json()
+        data += res_json['data']
+        next_page = res_json['paging']['next']   
+    k_print(f"Total Videos: {total_videos} - Scrapped Total Videos: {len(data)} for location: {kwargs['location']}", log_level='info')
+    if len(data) != total_videos:
+        k_print(f"Total video and Scrapped videos mismatched for location: {kwargs['location']}", log_level='error')
+        return
+    matched_csv_data = []
+    unmatched_csv_data = []
+    for video in data:
+        name, video_url = video['name'], video['link']
+        matched_data = [d for d in master_data if name.strip() in d['OWNER NAME (BANGLA)'].strip() and d['LOCATION'].strip().upper() == kwargs['location'].strip().upper()]
+        if matched_data:
+            data = {'code': matched_data[0]['CODE'], 'name_english': matched_data[0]['OWNER NAME (ENGLISH)'], 'name_bangla': matched_data[0]['OWNER NAME (BANGLA)'], 'location': matched_data[0]['LOCATION'], 'video_url': video_url}
+            matched_csv_data.append(data)
+            master_data.remove(matched_data[0])
+        else:
+            unmatched_csv_data.append({'video_title': name, 'video_url': video_url})
+
+    k_print(f"Matched Videos: {len(matched_csv_data)} - Unmatched Videos: {len(unmatched_csv_data)} for location: {kwargs['location']}", log_level='info')
+    with open(os.path.join(matched_output_dir, f'{location}_matched.csv'), 'w', newline='', encoding='utf-8') as f:
+        fieldnames = ['code', 'name_english', 'name_bangla', 'location', 'video_url']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for data in matched_csv_data:
+            writer.writerow(data)
+        k_print(f'Matched data written to {location}_matched.csv', log_level='info')
+    with open(os.path.join(unmatched_output_dir, f'{location}_unmatched.csv'), 'w', newline='', encoding='utf-8') as f:
+        fieldnames = ['video_title', 'video_url']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for data in unmatched_csv_data:
+            writer.writerow(data)
+        k_print(f'Unmatched data written to {location}_unmatched.csv', log_level='info')
+
 if __name__ == '__main__':
     master_csv_path = r"F:\KOLY\Others\ExperimetalProj\UniDostiQrGen\master_guest_list.csv"
     video_path = r"F:\KOLY\Others\ExperimetalProj\UniDostiQrGen\videos"
@@ -97,4 +169,6 @@ if __name__ == '__main__':
     # res = vClient.post('/me/projects', data={'name': 'koly'})
     # res = vClient.get('/me/projects/21393145')
     # res = vClient.put('/me/projects/21393145/videos/973000088')
-    print(res.status_code, res.text)
+    # print(res.status_code, res.text)
+
+    get_videos_info(location='CHITTAGONG', master_csv_path=master_csv_path)
